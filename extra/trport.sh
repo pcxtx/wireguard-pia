f5fd7d30e65254c9a03bb41646f28187c366c346
pcxtx <119761872+pcxtx@users.noreply.github.com>
2024-03-18 18:51:29 +0200

    Update trport.sh

--- extra/trport.sh
+++ extra/trport.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+trap 'exit 0' SIGTERM
+
+OLDPORT=0
+
+PORT=0
+
+while true
+do
+        [ -r "/pia-shared/port.dat" ] && PORT=$(cat /pia-shared/port.dat)
+
+        if [ $OLDPORT -ne $PORT ]; then
+                echo "Setting Transmission port to ($PORT)."
+                transmission-remote http://192.168.1.122:9092/transmission -p $PORT
+                OLDPORT=$PORT
+        fi
+
+        inotifywait --event modify --event create /pia-shared
+done
